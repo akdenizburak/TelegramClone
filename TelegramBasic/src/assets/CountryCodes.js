f2be70c6c77b9eb40f2515618e6a3fa8b11a7740
akdenizburak <akdenizburakk7@gmail.com>
2022-08-31 16:39:56 +0300

    Created CountryCodes.js

---
+++ TelegramBasic/src/assets/CountryCodes.js
@@ -0,0 +1,69 @@
+const CountryCodes = [
+    {
+        "name": "Afghanistan",
+        "dial_code": "+93",
+        "code": "AF"
+    },
+    {
+        "name": "Aland Islands",
+        "dial_code": "+358",
+        "code": "AX"
+    },
+    {
+        "name": "Albania",
+        "dial_code": "+355",
+        "code": "AL"
+    },
+    {
+        "name": "Algeria",
+        "dial_code": "+213",
+        "code": "DZ"
+    },
+    {
+        "name": "AmericanSamoa",
+        "dial_code": "+1684",
+        "code": "AS"
+    },
+    {
+        "name": "Andorra",
+        "dial_code": "+376",
+        "code": "AD"
+    },
+    {
+        "name": "Angola",
+        "dial_code": "+244",
+        "code": "AO"
+    },
+    {
+        "name": "Anguilla",
+        "dial_code": "+1264",
+        "code": "AI"
+    },
+    {
+        "name": "Antarctica",
+        "dial_code": "+672",
+        "code": "AQ"
+    },
+    {
+        "name": "Antigua and Barbuda",
+        "dial_code": "+1268",
+        "code": "AG"
+    },
+    {
+        "name": "Argentina",
+        "dial_code": "+54",
+        "code": "AR"
+    },
+    {
+        "name": "Armenia",
+        "dial_code": "+374",
+        "code": "AM"
+    },
+    {
+        "name": "Aruba",
+        "dial_code": "+297",
+        "code": "AW"
+    }
+]
+
+export default CountryCodes;
